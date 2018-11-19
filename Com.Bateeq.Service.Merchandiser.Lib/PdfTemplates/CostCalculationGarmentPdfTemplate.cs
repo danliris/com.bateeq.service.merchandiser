00ba5f48fbebdf506829044fb12f8f2ec32357ed
Andri Sibarani <andri.sebastian@moonlay.com>
2018-11-19 15:40:10 +0700

    Merchandiser - PDF Template -
    Ganti Direkur Penjualan jadi Direktur Marketing
    a.n pak haenis gunarto

--- Com.Bateeq.Service.Merchandiser.Lib/PdfTemplates/CostCalculationGarmentPdfTemplate.cs
+++ Com.Bateeq.Service.Merchandiser.Lib/PdfTemplates/CostCalculationGarmentPdfTemplate.cs
@@ -412,7 +412,7 @@ namespace Com.Bateeq.Service.Merchandiser.Lib.PdfTemplates
 
             cell_signature.Phrase = new Phrase("Merchandiser", normal_font);
             table_signature.AddCell(cell_signature);
-            cell_signature.Phrase = new Phrase("Direktur Penjualan", normal_font);
+            cell_signature.Phrase = new Phrase("Direktur Marketing", normal_font);
             table_signature.AddCell(cell_signature);
             #endregion
 

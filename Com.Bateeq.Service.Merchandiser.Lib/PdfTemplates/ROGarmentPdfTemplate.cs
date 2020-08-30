b5265a3c4d0a5d8b4efe3a752bedcc771e0020c2
danliris <danliris.cloud@gmail.com>
2020-08-30 15:33:43 +0700

    Update ROGarmentPdfTemplate.cs

--- Com.Bateeq.Service.Merchandiser.Lib/PdfTemplates/ROGarmentPdfTemplate.cs
+++ Com.Bateeq.Service.Merchandiser.Lib/PdfTemplates/ROGarmentPdfTemplate.cs
@@ -764,8 +764,8 @@ namespace Com.Bateeq.Service.Merchandiser.Lib.PdfTemplates
             table_signature.AddCell(cell_signature);
             cell_signature.Phrase = new Phrase("Mengetahui", normal_font);
             table_signature.AddCell(cell_signature);
-            cell_signature.Phrase = new Phrase("Menyetujui", normal_font);
-            table_signature.AddCell(cell_signature);
+            //cell_signature.Phrase = new Phrase("Menyetujui", normal_font);
+            //table_signature.AddCell(cell_signature);
 
             cell_signature_noted.Phrase = new Phrase("(                           )", normal_font);
             table_signature.AddCell(cell_signature_noted);
@@ -777,8 +777,8 @@ namespace Com.Bateeq.Service.Merchandiser.Lib.PdfTemplates
             table_signature.AddCell(cell_signature_noted);
             cell_signature_noted.Phrase = new Phrase("(Bekti Wahyuningsih)", normal_font);
             table_signature.AddCell(cell_signature_noted);
-            cell_signature_noted.Phrase = new Phrase("(Michelle Tjokrosaputro)", normal_font);
-            table_signature.AddCell(cell_signature_noted);
+            //cell_signature_noted.Phrase = new Phrase("(Michelle Tjokrosaputro)", normal_font);
+            //table_signature.AddCell(cell_signature_noted);
 
             float table_signatureY = rowYRoImage - imageRoHeight - 10;
             table_signature.WriteSelectedRows(0, -1, 10, table_signatureY, cb);

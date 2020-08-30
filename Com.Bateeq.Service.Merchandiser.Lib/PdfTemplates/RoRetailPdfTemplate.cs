1735585c5165444815ae25a40620f4c24cd1675e
danliris <danliris.cloud@gmail.com>
2020-08-30 16:05:45 +0700

    Update RoRetailPdfTemplate.cs

--- Com.Bateeq.Service.Merchandiser.Lib/PdfTemplates/RoRetailPdfTemplate.cs
+++ Com.Bateeq.Service.Merchandiser.Lib/PdfTemplates/RoRetailPdfTemplate.cs
@@ -836,7 +836,7 @@ namespace Com.Bateeq.Service.Merchandiser.Lib.PdfTemplates
 
             #region Signature (Bottom, Column 1.2)
 
-            PdfPTable table_signature = new PdfPTable(6);
+            PdfPTable table_signature = new PdfPTable(5);
             table_signature.TotalWidth = 570f;
 
             float[] signature_widths = new float[] { 1f, 1f, 1f, 1f, 1f, 1f };
